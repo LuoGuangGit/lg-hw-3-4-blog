336c0729ce5440900f078caed6529afbf5285c7d
Valen <wang_wenl@worksap.co.jp>
2020-10-30 12:10:20 +0800

    use element-ui and macon-editor

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import 'mavon-editor/dist/markdown/github-markdown.min.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  Vue.use(ElementUI)
+  Vue.use(mavonEditor)
 }
